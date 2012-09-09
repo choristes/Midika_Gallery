6e2b344e1bcf146e152e8760f7af1841f2f9d1f8
JoshuaZ <t@toru.ca>
2012-09-09 15:27:44 +0800

    Add basic UI (for smart phone), apply LESS, add Google Web Font, remove doc files of h5bp

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+$(document).ready(function () {
+    function resizeHandler () {
+        $('.view').height($(document).height() - $('header').height() * 2);
+        $('.photo-item').width(window.innerWidth * 0.75)
+            .first().css('margin-left', window.innerWidth * 0.125 + 'px')
+            .end()
+            .last().css('margin-right', window.innerWidth * 0.125 + 'px');
+        $('.photo-frame').height($('.photo-frame').width());
+        $('.photo-pic-overlay').height($('.photo-pic-overlay').width());
+    };
 
+    resizeHandler();
+    $(window).bind('resize', resizeHandler);
+});
