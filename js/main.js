723da884d8a72008374c7420840e850cd08e1144
JoshuaZ <t@toru.ca>
2012-09-10 16:42:21 +0800

    add portrait/landscape mode support, optimize main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-$(document).ready(function () {
-    function resizeHandler () {
-        $('.view').height($(document).height() - $('header').height() * 2);
-        $('.photo-item').width(window.innerWidth * 0.75)
-            .first().css('margin-left', window.innerWidth * 0.125 + 'px')
-            .end()
-            .last().css('margin-right', window.innerWidth * 0.125 + 'px');
-        $('.photo-frame').height($('.photo-frame').width());
-        $('.photo-pic-overlay').height($('.photo-pic-overlay').width());
-    };
+$(function () {
 
-    resizeHandler();
-    $(window).bind('resize', resizeHandler);
+    /* View Code */
+    var helper = function () {
+        var self = this,
+            resizeTimer = null;
+
+        this.resizeHandler = function () {
+            $('.view').height($(document).height() - $('header').height() * 2);
+            $('.photo-item').width(window.innerWidth * 0.75)
+                .first().css('margin-left', window.innerWidth * 0.125 + 'px')
+                .end()
+                .last().css('margin-right', window.innerWidth * 0.125 + 'px');
+            $('.photo-frame').height($('.photo-frame').width());
+            $('.photo-pic-overlay').height($('.photo-pic-overlay').width());
+        };
+
+        this.resizeHandler();
+
+        $(window).bind('resize', function () {
+            clearTimeout(resizeTimer);
+            resizeTimer = setTimeout(self.resizeHandler, 100);
+        });
+    }();
+
+
+    /* Controller Code */
+
+
+
+    /* Test Code */
+    $(document).bind('touchstart', function(){console.log('touchstart')});
+    $(document).bind('touchmove', function(){console.log('touchmove')});
+    $(document).bind('touchend', function(){console.log('touchend')});
+    $(document).bind('mousedown', function(){console.log('mousedown')});
+    $(document).bind('mousemove', function(){console.log('mousemove')});
+    $(document).bind('mouseup', function(){console.log('mouseup')});
+
+
 });
+
